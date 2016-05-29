254607a9cfb1c462d083c4c9f926e0adfa045440
BhVaswani <BVaswani@my.harrisburgu.edu>
2016-05-29 03:14:43 -0700

    Added Javascript file for the website

---
+++ scripts/pageTransition.js
@@ -0,0 +1,31 @@
+
+
+$(document).ready(function () {
+$('#navTab').hide();
+$("#navTab").delay("slow").fadeIn();
+
+
+$('#Mainheader').click(function(){
+	$( this ).css('background-color','#0080ff');
+	$( this ).css('border-radius','5px');
+	$('#headText').css('color','white');
+});
+
+$( "#header1" ).click(function() {
+       $( "#seaWonder" ).toggle( "slow", function() {
+                        
+		});
+});
+
+$( "#header2" ).click(function() {
+       $( "#seaMove" ).toggle( "slow", function() {
+                        
+		});
+});
+
+
+
+
+});
+
+
